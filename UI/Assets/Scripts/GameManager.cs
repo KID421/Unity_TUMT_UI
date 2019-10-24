d77d705f541d9dd3a622dadcdbea21814d2a0c0d
KID <50666207+KID421@users.noreply.github.com>
2019-10-24 14:29:54 +0800

    新增欄位修飾詞並指定欄位

--- UI/Assets/Scripts/GameManager.cs
+++ UI/Assets/Scripts/GameManager.cs
@@ -4,6 +4,7 @@ using UnityEngine.Audio;    // 引用 音頻 API
 public class GameManager : MonoBehaviour
 {
     // 定義欄位 (宣告變數)
-    // 類型 名稱 結尾
-    AudioMixer mixer;
+    // 修飾詞 類型 名稱 結尾
+    // public 公開、private 私人
+    public AudioMixer mixer;
 }
