493e1c0b2657f76866c9535fecc1b8824738cdfe
KID <50666207+KID421@users.noreply.github.com>
2019-12-05 13:48:49 +0800

    定義暫停方法並利用更新事件呼叫

---
+++ UI/Assets/Scripts/PauseControl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseControl : MonoBehaviour
+{
+    [Header("暫停介面")]
+    public Image imagePause;
+    public Sprite spritePause, spritePlay;
+
+    /// <summary>
+    /// 暫停方法
+    /// </summary>
+    private void Pause()
+    {
+        print("暫停~");
+    }
+
+    // 更新：一秒執行約 60 次
+    private void Update()
+    {
+        Pause();
+    }
+}
