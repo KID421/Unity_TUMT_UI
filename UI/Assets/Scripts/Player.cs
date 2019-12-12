02363518a732862f2bc1188145a8cfa52a25d031
KID <50666207+KID421@users.noreply.github.com>
2019-12-12 14:32:21 +0800

    利用取得元件方法取得陷阱的傷害值成員，讓每個陷阱都有不同傷害

--- UI/Assets/Scripts/Player.cs
+++ UI/Assets/Scripts/Player.cs
@@ -9,10 +9,11 @@ public class Player : MonoBehaviour
     // 觸發事件：碰到勾選 IsTrigger 物件會執行一次
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "陷阱")    // 如果 碰到.標籤 等於 "陷阱"
+        if (other.tag == "陷阱")                        // 如果 碰到.標籤 等於 "陷阱"
         {
-            hp -= 10;               // 血量 扣 10
-            hpSlider.value = hp;    // 血量滑桿.值 = 血量
+            int d = other.GetComponent<Trap>().damage;  // 取得元件<泛型>().成員
+            hp -= d;                                    // 血量 扣 10
+            hpSlider.value = hp;                        // 血量滑桿.值 = 血量
         }
     }
 }
