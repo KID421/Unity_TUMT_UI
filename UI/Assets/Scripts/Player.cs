b9524716e70a8e66b23041fe207ac037bdb8f981
KID <50666207+KID421@users.noreply.github.com>
2019-12-12 13:27:59 +0800

    新增玩家腳本，使用觸發事件偵測是否碰到陷阱

---
+++ UI/Assets/Scripts/Player.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // 觸發事件：碰到勾選 IsTrigger 物件會執行一次
+    private void OnTriggerEnter(Collider other)
+    {
+        print(other.gameObject);
+    }
+}
