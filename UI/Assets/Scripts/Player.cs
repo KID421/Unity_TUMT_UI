cca73213c1dd29029da0acefd86c0640edc48ea7
KID <50666207+KID421@users.noreply.github.com>
2019-12-12 13:49:25 +0800

    更新血量、血量滑桿與判斷碰到陷阱物件後扣血並更新滑桿長度

--- UI/Assets/Scripts/Player.cs
+++ UI/Assets/Scripts/Player.cs
@@ -1,10 +1,18 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;           // 引用 介面 API
 
 public class Player : MonoBehaviour
 {
+    public int hp = 100;        // 血量
+    public Slider hpSlider;     // 血量滑桿
+
     // 觸發事件：碰到勾選 IsTrigger 物件會執行一次
     private void OnTriggerEnter(Collider other)
     {
-        print(other.gameObject);
+        if (other.tag == "陷阱")    // 如果 碰到.標籤 等於 "陷阱"
+        {
+            hp -= 10;               // 血量 扣 10
+            hpSlider.value = hp;    // 血量滑桿.值 = 血量
+        }
     }
 }
